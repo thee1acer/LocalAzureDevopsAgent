9fd8ef6ebe8949deffd8677dd83fd78fc7d0a117
Mpho Nkala <mphonkala.info@gmail.com>
2025-04-02 12:12:34 +0200

    testing cloud deployment triggered by master

--- Local Azure Devops Agent/Controllers/WeatherForecastController.cs
+++ Local Azure Devops Agent/Controllers/WeatherForecastController.cs
@@ -21,6 +21,8 @@ namespace Local_Azure_Devops_Agent.Controllers
 		[HttpGet(Name = "GetWeatherForecast")]
 		public IEnumerable<WeatherForecast> Get()
 		{
+			_logger.LogInformation("Running Get Function");
+			
 			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
 			{
 				Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
