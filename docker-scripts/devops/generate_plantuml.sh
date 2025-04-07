9c39acce9f89272fd5621709cd04b3d9a7ff928d
Mpho Nkala <mphonkala.info@gmail.com>
2025-04-07 13:27:40 +0200

    Commit 14db6c3c: captain willard - apocolypse now (1979)

--- docker-scripts/devops/generate_plantuml.sh
+++ docker-scripts/devops/generate_plantuml.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-# Exit on any error
 set -e
 
-# Define paths
 PUML_FILE="${WORKSPACE_DIR}/umbraco-models.puml"
 OUTPUT_FILE="${OUTPUT_DIR}/umbraco-models.png"
 
-# Ensure directories exist
+# ensure directories exist
 mkdir -p "$OUTPUT_DIR"
 
-# Check if Java is installed
+# check if Java is installed
 if ! command -v java &> /dev/null; then
   echo "Java is not installed. Installing Java..."
 fi
 
-# Download PlantUML JAR if not already present
+# download PlantUML JAR if not already present
 if [ ! -f "$PLANTUML_JAR" ]; then
   echo "Downloading PlantUML..."
   wget -O "$PLANTUML_JAR" "https://github.com/plantuml/plantuml/releases/latest/download/plantuml.jar"
 fi
 
-# Generate PNG using PlantUML JAR
+# generate PNG using PlantUML JAR
 java -jar "$PLANTUML_JAR" -DPLANTUML_DOT="/usr/bin/dot" -tpng "$PUML_FILE" -o "$OUTPUT_DIR"
 
-# Verify if the PNG file was created
+# verify if the PNG file was created
 if [ ! -f "${OUTPUT_FILE}" ]; then
   echo "Error: PNG generation failed. File not found: ${OUTPUT_FILE}"
   exit 1
